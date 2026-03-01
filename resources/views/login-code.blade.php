ae883eba3d9f22c94923389b08773822b40bff85
treeio <treeo@ads.d>
2026-03-01 19:05:24 +0500

    fix

--- resources/views/login-code.blade.php
+++ resources/views/login-code.blade.php
@@ -1967,26 +1967,6 @@
     }).catch(() => { });
   });
 </script>
-
-<!-- Meta Pixel Code -->
-<script>
-  !function (f, b, e, v, n, t, s) {
-    if (f.fbq) return; n = f.fbq = function () {
-      n.callMethod ?
-        n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-    };
-    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-    n.queue = []; t = b.createElement(e); t.async = !0;
-    t.src = v; s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s)
-  }(window, document, 'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1398096467952166');
-  fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1398096467952166&ev=PageView&noscript=1" /></noscript>
-<!-- End Meta Pixel Code -->
 <!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
@@ -1997,10 +1977,10 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '3803073376656262');
+fbq('init', '1557160135343162');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=3803073376656262&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=1557160135343162&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
